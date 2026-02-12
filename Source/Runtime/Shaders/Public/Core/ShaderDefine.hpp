4e8526245dcd42b47a884088b4907ca660d160db
mallory-scotton <mallory.scotton@epitech.eu>
2026-02-12 18:25:32 +0100

    add: implement FShaderDefine structure with name and value attributes

--- Source/Runtime/Shaders/Public/Core/ShaderDefine.hpp
+++ Source/Runtime/Shaders/Public/Core/ShaderDefine.hpp
@@ -2,12 +2,25 @@
 
 #pragma once
 
+#include "Container/BasicString.hpp"
+#include "Container/ContainerForward.hpp"
 #include "ShadersBuild.hpp"
 
 namespace GP::Shaders
 {
 
+/// \brief Represents a shader define, which consists of a name and a value.
 struct FShaderDefine
-{};
+{
+public:
+    FString name;    //<! The name of the shader define.
+    FString value;   //<! The value of the shader define.
+
+public:
+    /// \brief Equality operator for shader define.
+    /// \param other The other shader define to compare with.
+    /// \return True if both shader defines are equal, false otherwise.
+    GP_NODISCARD bool operator==(const FShaderDefine&) const = default;
+};
 
 }   // namespace GP::Shaders
