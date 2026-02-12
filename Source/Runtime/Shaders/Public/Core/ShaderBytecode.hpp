b581dee328c8e92fb7a85d33d79f897836939fd4
mallory-scotton <mallory.scotton@epitech.eu>
2026-02-12 18:22:43 +0100

    add: define FShaderBytecode structure with metadata and methods for shader management

--- Source/Runtime/Shaders/Public/Core/ShaderBytecode.hpp
+++ Source/Runtime/Shaders/Public/Core/ShaderBytecode.hpp
@@ -2,12 +2,31 @@
 
 #pragma once
 
+#include "Container/Array.hpp"
+#include "Container/BasicString.hpp"
+#include "Container/ContainerForward.hpp"
+#include "Core/ShaderHash.hpp"
+#include "Core/ShaderTypes.hpp"
 #include "ShadersBuild.hpp"
 
 namespace GP::Shaders
 {
 
+/// \brief FShaderBytecode encapsulates the compiled shader bytecode along with metadata such as the target shading
+/// language, shader stage, source hash, and entry point. This structure is used to represent the output of shader
+/// compilation and can be used for caching, debugging, and runtime shader management.
 struct FShaderBytecode
-{};
+{
+public:
+    Container::TArray<Byte> data;                      //<! Raw bytecode data for the shader
+    EShaderTarget target = EShaderTarget::SPIRV_1_5;   //<! Target shading language or intermediate representation
+    EShaderStage stage = EShaderStage::None;           //<! Shader stage this bytecode is intended for
+    FShaderHash sourceHash;   //<! Hash of the original shader source code, useful for caching and debugging
+    FString entryPoint;       //<! Entry point function name in the shader, typically "main"
+
+public:
+    GP_NODISCARD bool IsValid() const noexcept;
+    GP_NODISCARD SizeT GetSize() const noexcept;
+};
 
 }   // namespace GP::Shaders
