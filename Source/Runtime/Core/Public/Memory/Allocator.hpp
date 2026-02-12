d2d79928f97e1cbfdb50befb17c4d45f22067d78
mallory-scotton <mallory.scotton@epitech.eu>
2026-02-12 18:15:10 +0100

    add: implement TAllocator class and forward declaration in Memory module

---
+++ Source/Runtime/Core/Public/Memory/Allocator.hpp
@@ -0,0 +1,14 @@
+// Copyright (c) - Graphical Playground. All rights reserved.
+
+#pragma once
+
+#include "CoreBuild.hpp"
+
+namespace GP::Memory
+{
+
+template <typename T>
+class TAllocator
+{};
+
+}   // namespace GP::Memory
--- Source/Runtime/Core/Public/Memory/Memory.hpp
+++ Source/Runtime/Core/Public/Memory/Memory.hpp
@@ -1,3 +1,12 @@
 // Copyright (c) - Graphical Playground. All rights reserved.
 
 #pragma once
+
+// Core Build
+#include "CoreBuild.hpp"
+
+// Memory forward
+#include "Memory/MemoryForward.hpp"
+
+// Implementation
+#include "Memory/Allocator.hpp"
