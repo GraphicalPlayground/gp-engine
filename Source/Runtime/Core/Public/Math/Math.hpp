7985af5907af2cb021b8883d28f3ef27138f8250
mallory-scotton <mallory.scotton@epitech.eu>
2026-02-13 14:21:38 +0100

    add: include additional math headers for comprehensive mathematical operations

--- Source/Runtime/Core/Public/Math/Math.hpp
+++ Source/Runtime/Core/Public/Math/Math.hpp
@@ -1,3 +1,32 @@
 // Copyright (c) - Graphical Playground. All rights reserved.
 
 #pragma once
+
+// Core dependencies
+#include "CoreBuild.hpp"
+
+// Implementations
+#include "Math/Colors/Color.hpp"
+#include "Math/Colors/LinearColor.hpp"
+#include "Math/Geometry/AABB.hpp"
+#include "Math/Geometry/Box.hpp"
+#include "Math/Geometry/Frustum.hpp"
+#include "Math/Geometry/OBB.hpp"
+#include "Math/Geometry/Plane.hpp"
+#include "Math/Geometry/Ray.hpp"
+#include "Math/Geometry/Rectangle.hpp"
+#include "Math/Geometry/Sphere.hpp"
+#include "Math/MathForward.hpp"
+#include "Math/Matrix/Matrix2x2.hpp"
+#include "Math/Matrix/Matrix3x3.hpp"
+#include "Math/Matrix/Matrix3x4.hpp"
+#include "Math/Matrix/Matrix4x4.hpp"
+#include "Math/Rotation/DualQuaternion.hpp"
+#include "Math/Rotation/Quaternion.hpp"
+#include "Math/Rotation/Rotator.hpp"
+#include "Math/Rotation/Rotator2D.hpp"
+#include "Math/Utils/Constants.hpp"
+#include "Math/Utils/Linear.hpp"
+#include "Math/Vector/Vector2.hpp"
+#include "Math/Vector/Vector3.hpp"
+#include "Math/Vector/Vector4.hpp"
