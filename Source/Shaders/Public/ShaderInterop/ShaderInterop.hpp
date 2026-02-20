272ee65650312d2e8902885cc0510560f7a67894
mallory-scotton <mallory.scotton@epitech.eu>
2026-02-20 15:12:20 +0100

    feat: add ShaderInterop.hpp header file for shader interoperability

---
+++ Source/Shaders/Public/ShaderInterop/ShaderInterop.hpp
@@ -0,0 +1,12 @@
+// Copyright (c) - Graphical Playground. All rights reserved.
+
+#ifndef _SHADER_INTEROP_HPP_
+#define _SHADER_INTEROP_HPP_
+
+#ifdef __cplusplus   // C++ code
+
+#else                // HLSL code
+
+#endif
+
+#endif   // _SHADER_INTEROP_HPP_
