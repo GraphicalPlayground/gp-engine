35a0bb735c0095f9d0e1f7716470402895a571cf
mallory-scotton <mallory.scotton@epitech.eu>
2026-02-21 18:03:35 +0100

    feat: add type aliases for container types including TArray, TStaticArray, TSet, TMap, TList, TQueue, TDeque, and TSpan

--- Source/Runtime/Core/Public/Container/ContainerForward.hpp
+++ Source/Runtime/Core/Public/Container/ContainerForward.hpp
@@ -43,3 +43,21 @@ using FString = GP::Container::TBasicString<GP::Char8>;
 using FWString = GP::Container::TBasicString<GP::WideChar>;
 using FString16 = GP::Container::TBasicString<GP::Char16>;
 using FString32 = GP::Container::TBasicString<GP::Char32>;
+
+template <typename T, GP::Concepts::IsAllocator Alloc>
+using TArray = GP::Container::TArray<T, Alloc>;
+template <typename T, GP::SizeT N, GP::Concepts::IsAllocator Alloc>
+using TStaticArray = GP::Container::TStaticArray<T, N, Alloc>;
+template <typename T, GP::Concepts::IsAllocator Alloc>
+using TSet = GP::Container::TSet<T, Alloc>;
+template <typename Key, typename Value, GP::Concepts::IsAllocator Alloc>
+using TMap = GP::Container::TMap<Key, Value, Alloc>;
+template <typename T, GP::Concepts::IsAllocator Alloc>
+using TList = GP::Container::TList<T, Alloc>;
+template <typename T, GP::Concepts::IsAllocator Alloc>
+using TQueue = GP::Container::TQueue<T, Alloc>;
+template <typename T, GP::Concepts::IsAllocator Alloc>
+using TDeque = GP::Container::TDeque<T, Alloc>;
+
+template <typename T, GP::SizeT Extent>
+using TSpan = GP::Container::TSpan<T, Extent>;
