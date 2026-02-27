221bb0e574bdb6b111e1376bd9fe4c8c9531c86d
mallory-scotton <mallory.scotton@epitech.eu>
2026-02-28 00:30:06 +0100

    feat: Create Events.hpp to include event-related class headers

---
+++ Source/Runtime/Core/Public/Events/Events.hpp
@@ -0,0 +1,9 @@
+// Copyright (c) - Graphical Playground. All rights reserved.
+
+#pragma once
+
+#include "Events/Delegate.hpp"
+#include "Events/DelegateHandle.hpp"
+#include "Events/EventsForward.hpp"
+#include "Events/MulticastDelegate.hpp"
+#include "Events/ScopedDelegateHandle.hpp"
