142025847ea98beeee161d5beac7885b539b204e
mallory-scotton <mallory.scotton@epitech.eu>
2026-02-28 00:29:04 +0100

    feat: Add EventForward.hpp for event delegation classes

---
+++ Source/Runtime/Core/Public/Events/EventForward.hpp
@@ -0,0 +1,19 @@
+// Copyright (c) - Graphical Playground. All rights reserved.
+
+#pragma once
+
+#include "CoreBuild.hpp"
+
+namespace GP::Events
+{
+
+class FDelegateHandle;
+class FScopedDelegateHandle;
+
+template <typename Signature>
+class TDelegate;
+
+template <typename Signature>
+class TMulticastDelegate;
+
+}   // namespace GP::Events
