172d4dfcbd80d11fdeb13543b1455b1371088b8c
mallory-scotton <mallory.scotton@epitech.eu>
2026-02-28 00:47:29 +0100

    feat: Add primary template for event traits to define associated event configurations

--- Source/Runtime/Core/Public/Events/EventsForward.hpp
+++ Source/Runtime/Core/Public/Events/EventsForward.hpp
@@ -17,3 +17,16 @@ template <typename Signature>
 class TMulticastDelegate;
 
 }   // namespace GP::Events
+
+namespace GP
+{
+
+/// \brief Primary template for event traits. Specialize this template for each event type to define associated traits
+/// such as the delegate signature, handle types, and other event-specific configurations.
+/// \tparam ObjectType The type of the object that owns or is associated with the event. This is typically a class
+/// type for member events, but can be void or any other type for free-function or static events.
+template <typename ObjectType>
+struct FEvents
+{};
+
+}   // namespace GP
