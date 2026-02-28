ad2380f4e72a62a958c3633e023e2f3aecae0f47
mallory-scotton <mallory.scotton@epitech.eu>
2026-02-28 23:21:07 +0100

    feat: Add SDLWindow constructor and event dispatching methods for improved window management

--- Source/Runtime/HAL/Private/SDL/SDLWindow.cpp
+++ Source/Runtime/HAL/Private/SDL/SDLWindow.cpp
@@ -1,6 +1,7 @@
 // Copyright (c) - Graphical Playground. All rights reserved.
 
 #include "SDL/SDLWindow.hpp"
+#include "SDL/SDLWindowingSystem.hpp"
 
 namespace GP::HAL
 {
@@ -15,6 +16,21 @@ SDLWindow::SDLWindow()
     }
 }
 
+SDLWindow::SDLWindow(const FWindowDesc& desc, SDLWindowingSystem* owner)
+{
+    GP_UNUSED(owner);
+    GP_UNUSED(desc);
+}
+
 SDLWindow::~SDLWindow() { /*Close();*/ }
+
+void SDLWindow::DispatchSDLEvent(const SDL_Event& event) { GP_UNUSED(event); }
+
+GP_NODISCARD SDL_Window* SDLWindow::GetSDLWindow() const noexcept { return m_window; }
+
+GP_NODISCARD SDL_WindowID SDLWindow::GetSDLWindowID() const noexcept
+{
+    return (m_window != nullptr) ? SDL_GetWindowID(m_window) : 0;
+}
 
 }   // namespace GP::HAL
