02719e0905887ee579132a2122dd1e1f8ca807af
mallory-scotton <mallory.scotton@epitech.eu>
2026-02-28 23:20:54 +0100

    feat: Add includes for IWindowingSystem and WindowDesc in Windowing.hpp

--- Source/Runtime/HAL/Public/Windowing/Windowing.hpp
+++ Source/Runtime/HAL/Public/Windowing/Windowing.hpp
@@ -3,5 +3,7 @@
 #pragma once
 
 #include "Windowing/IWindow.hpp"
+#include "Windowing/IWindowingSystem.hpp"
+#include "Windowing/WindowDesc.hpp"
 #include "Windowing/WindowEnums.hpp"
 #include "Windowing/WindowEvents.hpp"
