c663c959eb3857159c96134d0324a730fcd2d34e
mallory-scotton <mallory.scotton@epitech.eu>
2026-03-01 18:48:01 +0100

    feat: Create Utilities header to include StringFormat and StringUtilities

---
+++ Source/Runtime/Core/Public/Utilities/Utilities.hpp
@@ -0,0 +1,10 @@
+// Copyright (c) - Graphical Playground. All rights reserved.
+
+#pragma once
+
+// Main Core
+#include "CoreBuild.hpp"
+
+// Submodule headers
+#include "Utilities/StringFormat.hpp"
+#include "Utilities/StringUtilities.hpp"
