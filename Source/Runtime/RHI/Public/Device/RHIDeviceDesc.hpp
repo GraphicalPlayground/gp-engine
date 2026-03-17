83b5b0ec2ddec2f68d921114f641db06fc257dfa
mallory-scotton <mallory.scotton@epitech.eu>
2026-03-17 21:02:20 +0100

    add: create RHIDeviceDesc.hpp for device description structure

---
+++ Source/Runtime/RHI/Public/Device/RHIDeviceDesc.hpp
@@ -0,0 +1,14 @@
+// Copyright (c) - Graphical Playground. All rights reserved.
+
+#pragma once
+
+#include "RHIBuild.hpp"
+
+namespace GP
+{
+
+/// @brief
+struct RHIDeviceDesc
+{};
+
+}   // namespace GP
