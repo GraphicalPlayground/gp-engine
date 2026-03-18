a43d1a7c231edf8d08bd4430aa77c03d3d86d977
mallory-scotton <mallory.scotton@epitech.eu>
2026-03-18 10:10:39 +0100

    refactor: enhance IWindowSystem interface with additional display and event handling methods

--- Source/Runtime/HAL/Public/Window/IWindowSystem.hpp
+++ Source/Runtime/HAL/Public/Window/IWindowSystem.hpp
@@ -2,7 +2,10 @@
 
 #pragma once
 
+#include "Container/Views/ArrayView.hpp"
+#include "Container/Views/StringView.hpp"
 #include "CoreBuild.hpp"
+#include "Display/IDisplay.hpp"
 #include "Memory/SharedPtr.hpp"
 #include "Memory/UniquePtr.hpp"
 #include "Memory/WeakPtr.hpp"
@@ -19,12 +22,47 @@ public:
     virtual ~IWindowSystem() = default;
 
 public:
-    virtual TSharedPtr<IWindow> CreateWindow(const FWindowDesc& desc) noexcept = 0;
+    ///
+    /// @section Window Management
+    ///
+
+    GP_NODISCARD virtual TSharedPtr<IWindow> CreateWindow(const FWindowDesc& desc) noexcept = 0;
+    GP_NODISCARD virtual TArrayView<const TSharedPtr<IWindow>> GetWindows() const noexcept = 0;
+    GP_NODISCARD virtual TSharedPtr<IWindow> GetFocusedWindow() const noexcept = 0;
+    GP_NODISCARD virtual TSharedPtr<IWindow> GetMainWindow() const noexcept = 0;
+    GP_NODISCARD virtual SizeT GetWindowCount() const noexcept = 0;
+    GP_NODISCARD virtual bool HasWindows() const noexcept = 0;
+    GP_NODISCARD virtual TSharedPtr<IWindow> FindWindowByNativeHandle(void* nativeHandle) const noexcept = 0;
+
+    ///
+    /// @section Display Management
+    ///
+
+    GP_NODISCARD virtual TSharedPtr<const IDisplay> GetPrimaryDisplay() const noexcept = 0;
+    GP_NODISCARD virtual TArrayView<const TSharedPtr<const IDisplay>> GetDisplays() const noexcept = 0;
+    GP_NODISCARD virtual SizeT GetDisplayCount() const noexcept = 0;
+    GP_NODISCARD virtual bool HasDisplays() const noexcept = 0;
+    GP_NODISCARD virtual TSharedPtr<const IDisplay> FindDisplayById(SizeT id) const noexcept = 0;
+    GP_NODISCARD virtual TSharedPtr<const IDisplay> FindDisplayByNativeHandle(void* nativeHandle) const noexcept = 0;
+    GP_NODISCARD virtual TSharedPtr<const IDisplay>
+        FindDisplayForWindow(const TSharedPtr<IWindow>& window) const noexcept = 0;
+    virtual void RefreshDisplayList() noexcept = 0;
+
+    ///
+    /// @section Event Handling
+    ///
+
     virtual void PollEvents() noexcept = 0;
     virtual void DispatchEvents() noexcept = 0;
 
+    ///
+    /// @section Utility
+    ///
+
+    GP_NODISCARD virtual FStringView GetBackendName() const noexcept = 0;
+
 public:
-    static TUniquePtr<IWindowSystem> Create() noexcept;
+    GP_NODISCARD static TUniquePtr<IWindowSystem> Create() noexcept;
 };
 
 }   // namespace GP
