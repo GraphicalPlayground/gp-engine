55dca1601e8613aae1aefb7e0a2e8ab938750ea0
mallory-scotton <mallory.scotton@epitech.eu>
2026-03-19 23:59:10 +0100

    fix: update size checks in StaticArray tests to use unsigned long long for consistency

--- Source/Runtime/Core/Tests/Container/Sequential/StaticArray.Tests.cpp
+++ Source/Runtime/Core/Tests/Container/Sequential/StaticArray.Tests.cpp
@@ -13,19 +13,19 @@ TEST_CASE("StaticArray Construction", "[GP][Core][Container][TStaticArray]")
     SECTION("Default Constructor")
     {
         TStaticArray<int, 5> arr;
-        REQUIRE(arr.Size() == 5);
+        REQUIRE(arr.Size() == 5ull);
         for (SizeT i = 0; i < arr.Size(); ++i) { REQUIRE(arr[i] == 0); }
     }
     SECTION("Constructor with size and default value")
     {
         TStaticArray<int, 5> arr(42);
-        REQUIRE(arr.Size() == 5);
+        REQUIRE(arr.Size() == 5ull);
         for (SizeT i = 0; i < arr.Size(); ++i) { REQUIRE(arr[i] == 42); }
     }
     SECTION("COnstructor with initializer list")
     {
         TStaticArray<int, 3> arr = { 1, 2, 3 };
-        REQUIRE(arr.Size() == 3);
+        REQUIRE(arr.Size() == 3ull);
         REQUIRE(arr[0] == 1);
         REQUIRE(arr[1] == 2);
         REQUIRE(arr[2] == 3);
@@ -34,13 +34,13 @@ TEST_CASE("StaticArray Construction", "[GP][Core][Container][TStaticArray]")
     {
         TArray<int> vec = { 1, 2, 3 };
         TStaticArray<int, 3> arr(vec.begin(), vec.end());
-        REQUIRE(arr.Size() == 3);
+        REQUIRE(arr.Size() == 3ull);
     }
     SECTION("Copy Constructor")
     {
         TStaticArray<int, 3> arr1 = { 1, 2, 3 };
         TStaticArray<int, 3> arr2 = arr1;
-        REQUIRE(arr2.Size() == 3);
+        REQUIRE(arr2.Size() == 3ull);
         REQUIRE(arr2[0] == 1);
         REQUIRE(arr2[1] == 2);
         REQUIRE(arr2[2] == 3);
@@ -49,14 +49,14 @@ TEST_CASE("StaticArray Construction", "[GP][Core][Container][TStaticArray]")
     {
         TStaticArray<int, 0> arr1;
         TStaticArray<int, 0> arr2 = arr1;
-        REQUIRE(arr2.Size() == 0);
+        REQUIRE(arr2.Size() == 0ull);
         REQUIRE(arr2.IsEmpty());
     }
     SECTION("Move Constructor")
     {
         TStaticArray<int, 3> arr1 = { 1, 2, 3 };
         TStaticArray<int, 3> arr2 = std::move(arr1);
-        REQUIRE(arr2.Size() == 3);
+        REQUIRE(arr2.Size() == 3ull);
         REQUIRE(arr2[0] == 1);
         REQUIRE(arr2[1] == 2);
         REQUIRE(arr2[2] == 3);
