2dc702cf9921ce7df46620cb92ec6eacee44539b
mallory-scotton <mallory.scotton@epitech.eu>
2026-04-16 20:11:43 +0200

    feat: standardize module names and improve CMake configuration across launch and runtime components

--- source/launch/editor/private/Main.cpp
+++ source/launch/editor/private/Main.cpp
@@ -1,7 +1,10 @@
 // Copyright (c) - Graphical Playground. All rights reserved.
 
+#include <iostream>
+
 int main()
 {
+    std::cout << "Hello, Graphical Playground Editor!" << std::endl;
     // TODO: Implement editor main function.
     return 0;
 }
