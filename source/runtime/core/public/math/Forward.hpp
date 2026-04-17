1f97c69a926ebeaf5dcceafd3ed0252fce0f6ad9
mallory-scotton <mallory.scotton@epitech.eu>
2026-04-17 11:09:23 +0200

    feat: add mathematical structures for vector and matrix types in gp::math namespace

--- source/runtime/core/public/math/Forward.hpp
+++ source/runtime/core/public/math/Forward.hpp
@@ -1,3 +1,39 @@
 // Copyright (c) - Graphical Playground. All rights reserved.
 
 #pragma once
+
+#include "CoreMinimal.hpp"
+#include <concepts>
+
+namespace gp::math
+{
+
+template <std::floating_point T>
+struct Vec2;
+template <std::floating_point T>
+struct Vec3;
+template <std::floating_point T>
+struct Vec4;
+
+template <std::floating_point T>
+struct Mat2;
+template <std::floating_point T>
+struct Mat3;
+template <std::floating_point T>
+struct Mat34;
+template <std::floating_point T>
+struct Mat4;
+
+template <std::floating_point T>
+struct Quat;
+template <std::floating_point T>
+struct DualQuat;
+template <std::floating_point T>
+struct Rotator;
+template <std::floating_point T>
+struct Rotator2;
+
+template <std::floating_point T>
+struct Transform;
+
+} // namespace gp::math
