c32bc05113b67ace833ef3e30260e76fbb332e41
mallory-scotton <mallory.scotton@epitech.eu>
2026-04-17 09:13:19 +0200

    chore: clean up TODO comments in gp-build-tool.cmake for clarity and focus

--- cmake/gp-build-tool.cmake
+++ cmake/gp-build-tool.cmake
@@ -9,12 +9,8 @@ include(gp-build-tool/gp-flags)
 
 # TODO:
 #  - Add support for custom build steps (e.g., code generation, asset processing)
-#  - Add support for adding definitons, includes, and dependencies to specific targets
 #  - Add different type of dependencies: static, shared, interface
 #  - Add support for custom build configurations (e.g., Debug, Release, etc.)
-#  - Add support for custom build options (e.g., enabling/disabling features, setting compiler flags, etc.)
 #  - Add support for custom build targets (e.g., tests, benchmarks, etc.)
 #  - Add support for custom build tools (e.g., code formatters, linters, etc.)
-#  - Enhance module ordering to ensure that dependencies are built before the targets that depend on them
-#  - Add the custom register/configure systems for targets
 #  - Write documentation for the GP Build Tool, including usage examples and best practices
