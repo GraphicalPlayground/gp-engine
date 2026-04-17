ed58038b08fdc69b13bce35f23edf3fe9b8a01c9
mallory-scotton <mallory.scotton@epitech.eu>
2026-04-17 11:11:11 +0200

    chore: reorder includes and forward declarations in CoreForward.hpp for better organization

--- source/runtime/core/public/CoreForward.hpp
+++ source/runtime/core/public/CoreForward.hpp
@@ -1,3 +1,14 @@
 // Copyright (c) - Graphical Playground. All rights reserved.
 
 #pragma once
+
+// Include CoreMinimal
+#include "CoreMinimal.hpp"
+
+// Forward declarations
+#include "container/Forward.hpp"
+#include "crypto/Forward.hpp"
+#include "errors/Forward.hpp"
+#include "math/Forward.hpp"
+#include "memory/Forward.hpp"
+#include "utils/Forward.hpp"
