f682d411cac1d291bc3612f8f29b1f5c59da0c26
mallory-scotton <mallory.scotton@epitech.eu>
2026-04-22 16:21:00 +0200

    add: adding specialization for reference counting of the various RHI types

--- source/runtime/rhi/base/public/RHIForward.hpp
+++ source/runtime/rhi/base/public/RHIForward.hpp
@@ -4,6 +4,8 @@
 
 #pragma once
 
+#include "memory/RefCountPtr.hpp"
+
 namespace gp::rhi
 {
 
@@ -77,5 +79,50 @@ class WorkGraphShader;
 class StreamSourceSlot;
 
 struct UniformBufferLayout;
+
+/// @section Pointers
+
+using AmplificationShaderRef = gp::RefCountPtr<AmplificationShader>;
+using BlendStateRef = gp::RefCountPtr<BlendState>;
+using BoundShaderStateRef = gp::RefCountPtr<BoundShaderState>;
+using BufferRef = gp::RefCountPtr<Buffer>;
+using ComputePipelineStateRef = gp::RefCountPtr<ComputePipelineState>;
+using ComputeShaderRef = gp::RefCountPtr<ComputeShader>;
+using CustomPresentRef = gp::RefCountPtr<CustomPresent>;
+using DepthStencilStateRef = gp::RefCountPtr<DepthStencilState>;
+using GeometryShaderRef = gp::RefCountPtr<GeometryShader>;
+using GPUFenceRef = gp::RefCountPtr<GPUFence>;
+using GraphicsPipelineStateRef = gp::RefCountPtr<GraphicsPipelineState>;
+using MeshShaderRef = gp::RefCountPtr<MeshShader>;
+using PixelShaderRef = gp::RefCountPtr<PixelShader>;
+using RasterizerStateRef = gp::RefCountPtr<RasterizerState>;
+using RayTracingGeometryRef = gp::RefCountPtr<RayTracingGeometry>;
+using RayTracingPipelineStateRef = gp::RefCountPtr<RayTracingPipelineState>;
+using RayTracingSceneRef = gp::RefCountPtr<RayTracingScene>;
+using RayTracingShaderRef = gp::RefCountPtr<RayTracingShader>;
+using ShaderBindingTableRef = gp::RefCountPtr<ShaderBindingTable>;
+using RenderQueryPoolRef = gp::RefCountPtr<RenderQueryPool>;
+using RenderQueryRef = gp::RefCountPtr<RenderQuery>;
+using RHIResourceCollectRef = gp::RefCountPtr<ResourceCollection>;
+using RHIShaderLibrRef = gp::RefCountPtr<ShaderLibrary>;
+using RHIShaderResourceVRef = gp::RefCountPtr<ShaderResourceView>;
+using SamplerStateRef = gp::RefCountPtr<SamplerState>;
+using ShaderResourceViewRef = gp::RefCountPtr<ShaderResourceView>;
+using ShaderBundleRef = gp::RefCountPtr<ShaderBundle>;
+using StagingBufferRef = gp::RefCountPtr<StagingBuffer>;
+using TextureReferenceRef = gp::RefCountPtr<TextureReference>;
+using TextureRef = gp::RefCountPtr<Texture>;
+#if !defined(GP_RHI_NEW_GPU_PROFILER) || (GP_RHI_NEW_GPU_PROFILER == 0)
+using TimestampCalibrationQueryRef = gp::RefCountPtr<TimestampCalibrationQuery>;
+#endif
+using UniformBufferLayoutRef = gp::RefCountPtr<const UniformBufferLayout>;
+using UniformBufferRef = gp::RefCountPtr<UniformBuffer>;
+using UnorderedAccessViewRef = gp::RefCountPtr<UnorderedAccessView>;
+using VertexDeclarationRef = gp::RefCountPtr<VertexDeclaration>;
+using VertexShaderRef = gp::RefCountPtr<VertexShader>;
+using ViewportRef = gp::RefCountPtr<Viewport>;
+using WorkGraphPipelineStateRef = gp::RefCountPtr<WorkGraphPipelineState>;
+using StreamSourceSlotRef = gp::RefCountPtr<StreamSourceSlot>;
+using WorkGraphShaderRef = gp::RefCountPtr<WorkGraphShader>;
 
 }   // namespace gp::rhi
