d35b447c43934b44476ef4b289c2417632c0ef1d
mallory-scotton <mallory.scotton@epitech.eu>
2026-05-01 20:36:03 +0200

    example: adding base layout placeholder for example in the gp-engine repository

---
+++ examples/01-hello-window/source/Main.cpp
@@ -0,0 +1,12 @@
+// Copyright (c) - Graphical Playground. All rights reserved.
+// For more information, see https://graphical-playground/legal
+// mailto:support AT graphical-playground DOT com
+
+#include <iostream>
+
+int main()
+{
+    // TODO: Remove this and replace it with actual code that creates a window and displays "Hello, World!" in it.
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
