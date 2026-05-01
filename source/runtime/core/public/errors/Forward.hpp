e91d335559db23832f2e2ec1f09502495b70ac9a
mallory-scotton <mallory.scotton@epitech.eu>
2026-05-01 16:23:38 +0200

    add(forward): adding all classes to forward declarations of error

--- source/runtime/core/public/errors/Forward.hpp
+++ source/runtime/core/public/errors/Forward.hpp
@@ -15,25 +15,41 @@ struct AbortConfig;
 struct FilterConfig;
 struct ThreadConfig;
 struct ErrorSystemConfig;
+struct CrashReportConfig;
 
 // Sinks
 class Sink;
 class AbortSink;
 class BreakpointSink;
 class ConsoleSink;
+class CallbackSink;
 class DeferredSink;
 class FileSink;
 class MultiSink;
 class RateLimitedSink;
 class TelemetrySink;
+class FilteredSink;
+class SplitterSink;
+class BroadcastSink;
+class RingBufferSink;
+class BinarySink;
+class CrashReporter;
 
 // Forward declarations
 
+struct MetaEntry;
 class ErrorCode;
 class ErrorRecord;
 class ErrorSystem;
 class ErrorContext;
 struct ContextFrame;
 class ContextScope;
+struct ErrorStatistics;
+class ErrorSystem;
+struct ErrorEntry;
+class ErrorRegistry;
+class FilterPredicate;
+class ErrorScope;
+class ErrorGuard;
 
 }   // namespace gp::error
