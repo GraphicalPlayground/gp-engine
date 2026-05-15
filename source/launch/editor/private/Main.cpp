b5c08084bf26f1bb3625bdcf1e6a757eb48f3b84
mallory-scotton <mallory.scotton@epitech.eu>
2026-05-15 17:49:28 +0200

    add: initialize editor module with essential files and CMake configuration

---
+++ source/launch/editor/private/Main.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) - Graphical Playground. All rights reserved.
+// For more information, see https://graphical-playground/legal
+// mailto:support AT graphical-playground DOT com
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello, Editor!" << std::endl;
+    return 0;
+}
