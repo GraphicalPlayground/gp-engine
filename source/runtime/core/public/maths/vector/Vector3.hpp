073d38226061e806ad684b4e49e9c851de086f1f
mallory-scotton <mallory.scotton@epitech.eu>
2026-05-19 15:49:51 +0200

    add: adding base for the mathematical library

--- source/runtime/core/public/maths/vector/Vector3.hpp
+++ source/runtime/core/public/maths/vector/Vector3.hpp
@@ -3,3 +3,20 @@
 // mailto:support AT graphical-playground DOT com
 
 #pragma once
+
+#include "concepts/Concepts.hpp"
+#include "CoreMinimal.hpp"   // IWYU pragma: keep
+
+namespace gp::math
+{
+
+/// @brief A 3D vector template
+/// @tparam T The floating-point type for the vector components
+template <concepts::IsFloatingPoint T>
+struct Vector3
+{};
+
+}   // namespace gp::math
+
+// Include the implementation of the Vector3 template
+#include "maths/vector/Vector3.inl"
