f90c225a71d98ecad0ddcd7e19708294d7f69bcf
mallory-scotton <mallory.scotton@epitech.eu>
2026-05-27 10:36:22 +0200

    add: add HALForward header for windowing and input device classes

---
+++ source/runtime/hal/base/public/HALForward.hpp
@@ -0,0 +1,26 @@
+// Copyright (c) - Graphical Playground. All rights reserved.
+// For more information, see https://graphical-playground/legal
+// mailto:support AT graphical-playground DOT com
+
+#pragma once
+
+namespace gp::hal
+{
+
+/// @section Windowing and display
+
+class Window;
+class Display;
+class Monitor;
+
+struct VideoMode;
+struct WindowDesc;
+
+/// @section Input devices
+
+class Keyboard;
+class Mouse;
+class Gamepad;
+class TouchDevice;
+
+}   // namespace gp::hal
