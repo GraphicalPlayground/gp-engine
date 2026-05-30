2f104f6759f729a50335f0cc5837382f91ca703d
mallory-scotton <mallory.scotton@epitech.eu>
2026-05-30 10:35:58 +0200

    feat: add initial implementation of HAL generic module with CMake configuration and documentation

---
+++ source/runtime/hal/generic/public/window/GenericWindow.hpp
@@ -0,0 +1,16 @@
+// Copyright (c) - Graphical Playground. All rights reserved.
+// For more information, see https://graphical-playground/legal
+// mailto:support AT graphical-playground DOT com
+
+#pragma once
+
+#include "CoreMinimal.hpp"   // IWYU pragma: keep
+#include "window/IWindow.hpp"
+
+namespace gp::hal::generic
+{
+
+class Window : public gp::hal::IWindow
+{};
+
+}   // namespace gp::hal::generic
