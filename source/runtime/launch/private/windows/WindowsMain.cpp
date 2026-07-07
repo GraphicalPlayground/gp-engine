c9696382e7f7c4148d36b3ecaa5bf4bf396bd825
mallory-scotton <mallory.scotton@epitech.eu>
2026-07-07 19:07:58 +0200

    update: refactor launch entry points to use detail namespace

--- source/runtime/launch/private/windows/WindowsMain.cpp
+++ source/runtime/launch/private/windows/WindowsMain.cpp
@@ -6,6 +6,21 @@
 #include "Launch.hpp"
 #include <windows.h>
 
+namespace gp::detail
+{
+
+/// @brief Common entry point for both WinMain and main.
+/// @param[in] argc The number of command line arguments.
+/// @param[in] argv The command line arguments.
+/// @return The exit code of the application.
+inline int launch(int argc, char* argv[])
+{
+    return gp::launch(argc, argv);
+}
+
+}   // namespace gp::detail
+
+/// @brief GUI entry point for the application.
 int WINAPI WinMain(
     GP_MAYBE_UNUSED HINSTANCE hInstance,
     GP_MAYBE_UNUSED HINSTANCE hPrevInstance,
@@ -13,5 +28,11 @@ int WINAPI WinMain(
     GP_MAYBE_UNUSED int nCmdShow
 )
 {
-    return gp::launch(__argc, __argv);
+    return gp::detail::launch(__argc, __argv);
+}
+
+/// @brief Console entry point for the application.
+int main(int argc, char* argv[])
+{
+    return gp::detail::launch(argc, argv);
 }
