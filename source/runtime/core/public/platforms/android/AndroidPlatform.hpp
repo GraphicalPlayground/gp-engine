81f320d119976554bff7a71849a38d26917d9972
Mallow <mscotton.pro@gmail.com>
2026-09-12 14:27:35 +0900

    remove: remove useless definition of cnsole and mobile platform that aren't implemented yet

--- source/runtime/core/public/platforms/android/AndroidPlatform.hpp
+++
@@ -1,7 +0,0 @@
-// Copyright (c) - Graphical Playground. All rights reserved.
-// For more information, see https://graphical-playground/legal
-// mailto:support AT graphical-playground DOT com
-
-#pragma once
-
-#error "AndroidPlatform.hpp is not yet implemented. Please implement the Android platform support in this file."
--- source/runtime/core/public/platforms/ps4/PS4Platform.hpp
+++
@@ -1,7 +0,0 @@
-// Copyright (c) - Graphical Playground. All rights reserved.
-// For more information, see https://graphical-playground/legal
-// mailto:support AT graphical-playground DOT com
-
-#pragma once
-
-#error "PS4Platform.hpp is not yet implemented. Please implement the PS4 platform support in this file."
