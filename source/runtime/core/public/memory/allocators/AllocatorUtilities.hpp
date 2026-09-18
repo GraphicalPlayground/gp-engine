a721dda37ff3ef6ed20bfad072cb235a4586c2f9
Mallow <mscotton.pro@gmail.com>
2026-09-18 13:18:13 +0900

    add: create AllocatorUtilities.hpp with UntypedContainerElement definition

---
+++ source/runtime/core/public/memory/allocators/AllocatorUtilities.hpp
@@ -0,0 +1,14 @@
+// Copyright (c) - Graphical Playground. All rights reserved.
+// For more information, see https://graphical-playground.com/legal
+// mailto:support AT graphical-playground DOT com
+
+#pragma once
+
+namespace gp
+{
+
+/// @brief Opaque type used to represent container elements whose type is not known at compile time.
+struct UntypedContainerElement
+{};
+
+}   // namespace gp
